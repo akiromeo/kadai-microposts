a9c33c072556a499d3c78416bc5b7d9f40c7c5c6
akiyoshi.romeo <vw.golf.cab.range.rover.3rd@gmail.com>
2019-03-17 13:13:52 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
 
-  before_action :require_user_logged_in, only: [:index, :show]
-  
   def index
     @users = User.all.page(params[:page])
   end
@@ -11,11 +10,11 @@ class UsersController < ApplicationController
     @microposts = @user.microposts.order('created_at DESC').page(params[:page])
     counts(@user)
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
 
@@ -28,10 +27,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
 end
